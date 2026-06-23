b8099b1354e53a5f7aeae856c9b8a477d686ad9c
Marjorie-Pereira <marjorie300904@gmail.com>
2026-06-23 19:43:18 -0300

    Create playRound function with the inputs to play a single round of the game

--- script.js
+++ script.js
@@ -15,8 +15,48 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const choice = prompt('Make your choice (rock, paper or scissors):');
+  const choice = prompt("Make your choice (rock, paper or scissors):");
   return choice;
 }
 
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  console.log("Computer chose: ", computerChoice);
+  if (humanChoice.toLowerCase() === computerChoice) {
+    console.log("No one wins");
+    return;
+  }
+
+  switch (humanChoice.toLowerCase()) {
+    case "rock":
+      if (computerChoice === "scissors") {
+        humanScore += 1;
+        console.log("Rock beats Scissors, you win");
+      } else {
+        computerScore += 1;
+        console.log("Paper beats Rock, you lose");
+      }
+      break;
+    case "paper":
+      if (computerChoice === "scissors") {
+        computerScore += 1;
+        console.log("Scissors beats paper, you lose");
+      } else {
+        humanScore += 1;
+        console.log("Paper beats rock, you win");
+      }
+      break;
+    case "scissors":
+      if (computerChoice === "rock") {
+        computerScore += 1;
+        console.log("Rock beats scissors, you lose");
+      } else {
+        humanScore += 1;
+        console.log("Scissors beats paper, you win");
+      }
+      break;
+  }
+  console.log(`Computer: ${computerScore}. Human: ${humanScore}`);
+}
+
+
+
